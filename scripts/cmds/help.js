60890d2bd94eb718517fbd2165f31f08faa911c4
SISIRX3 <github2226@gmail.com>
2024-05-19 09:09:38 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🌟 | SISIR-𝗕𝗢𝗧 ]";
+const doNotDelete = "[ 🌟 | ✨SISIR-𝗕𝗢𝗧✨ ]";
 
 module.exports = {
   config: {
@@ -34,7 +34,7 @@ module.exports = {
       const categories = {};
       let msg = "";
 
-      msg += `╔═══════════╗\n     SISIR-𝗕𝗢𝗧🌟\n╚═══════════╝`;
+      msg += `╔═══════════╗\n     🌟SISIR-𝗕𝗢𝗧🌟\n╚═══════════╝`;
 
       for (const [name, value] of commands) {
           if (value.config.role > 1 && role < value.config.role) continue;
@@ -60,16 +60,16 @@ module.exports = {
 
       const totalCommands = commands.size;
       msg += `\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆, 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗵𝗮𝘀 ${totalCommands} 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝘁𝗵𝗮𝘁 𝗰𝗮𝗻 𝗯𝗲 𝘂𝘀𝗲𝗱\n`;
-      msg += `𝗧𝘆𝗽𝗲 ${prefix}𝗵𝗲𝗹𝗽 [SISIR-𝗕𝗢𝗧] 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 𝘁𝗵𝗮𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱\n`;
-      msg += `🌟 | SISIR-𝗕𝗢𝗧 `;
+      msg += `𝗧𝘆𝗽𝗲 ${prefix}𝗵𝗲𝗹𝗽 [𝗔𝗬𝗔𝗡-𝗕𝗢𝗧] 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 𝘁𝗵𝗮𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱\n`;
+      msg += `🌟 | ✨SISIR-𝗕𝗢𝗧✨ `;
 
 
       const helpListImages = [
-        "https://i.postimg.cc/8cvDpt37/images-17.jpg", // add image link here
-        "https://i.postimg.cc/qq2VVghn/received-430815183006013.jpg",
-        "https://i.postimg.cc/KzRxVZDr/received-455361183700405.jpg",
-        "https://i.postimg.cc/MGZW70cL/received-435752262736007.jpg",
-        "https://i.postimg.cc/Pq6d2LY5/received-3200033873462285.jpg",
+        "https://i.imgur.com/3ap4QdB.png", // add image link here
+        "https://i.imgur.com/RyPor2K.png",
+        "https://i.imgur.com/Td6POTk.png",
+        "https://i.imgur.com/VAFyQ4y.png",
+        "https://i.imgur.com/3TARwmi.png",
         // Add more image links as needed
       ];
 
@@ -133,4 +133,4 @@ function roleTextToString(roleText) {
     default:
       return "Unknown role";
   }
-}
+} 
