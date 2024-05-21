291a37f0cd1bf2fe5cbb648930a345a4cd796722
SISIRX3 <github2226@gmail.com>
2024-05-21 21:57:09 +0600

    Update sisir.js

--- scripts/cmds/sisir.js
+++ scripts/cmds/sisir.js
@@ -15,6 +15,6 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "sisir") return message.reply("আমাকে বলতে পারেন বস ঘুমাইছে-!!🥀");
+    if (event.body && event.body.toLowerCase() == "Sisir") return message.reply("আমাকে বলতে পারেন বস ঘুমাইছে-!!🥀");
 }
 }; 
